a4048f536dd7510840d84cf08844ae623d87f855
unknown <hanming@RD-HM-7627-01.wlyd.local>
2015-11-02 16:14:42 +0800

    add a test web

---
+++ Daker.Test/Daker.Test/Controllers/SayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Daker.Test.Controllers
+{
+    public class SayController : Controller
+    {
+        //
+        // GET: /Say/
+        public ActionResult Hello()
+        {
+            return View();
+        }
+	}
+}
