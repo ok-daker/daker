356d95a4805e6939b16f5ff02c810ceafcca5490
on-looker <694777627@qq.com>
2015-11-04 16:22:36 +0800

    添加缓存基本实现

---
+++ DChat/Framework/Cache/IRedisCache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Cache
+{
+    public interface IRedisCache
+    {
+        void Add<T>(string key, T value);
+        void Add<T>(string key, T value, TimeSpan span);
+        void AddToList(string key, string value);
+        T Get<T>(string key);
+        void Remove(string key);
+        void RemoveByRegex(string key);
+    }
+}
---
+++ DChat/Framework/Cache/RedisConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Cache
+{
+    public class RedisConfig : ConfigurationSection
+    {
+        [ConfigurationProperty("Host")]
+        public string Host
+        {
+            get
+            {
+                return this["Host"].ToString();
+            }
+            set
+            {
+                this["Host"] = value;
+            }
+        }
+        [ConfigurationProperty("InitialDB")]
+        public long InitialDB
+        {
+            get
+            {
+                return (long)this["InitialDB"];
+            }
+            set
+            {
+                this["InitialDB"] = value;
+            }
+        }
+        public int ConnectTimeout
+        {
+            get
+            {
+                return (int)this["ConnectTimeout"];
+            }
+            set
+            {
+                this["ConnectTimeout"] = value;
+            }
+
+        }
+        public string Port
+        {
+            get
+            {
+                return this["Port"].ToString();
+            }
+            set
+            {
+                this["Port"] = value;
+            }
+
+
+        }
+
+    }
+}
