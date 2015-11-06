ae4bbaf81586e7cc8fbd9e29b6e33029bc71a415
windfierce <449570293@qq.com>
2015-11-06 12:42:02 +0800

    日志框架添加

---
+++ DChat/Framework/Log/Log4NetLoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DChat.Framework.Log
+{
+    public class Log4NetLoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger(Type type)
+        {
+            return new Log4NetLogger(type);
+        }
+    }
+}
---
+++ DChat/Framework/Log/NullLoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DChat.Framework.Log
+{
+    public class NullLoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger(Type type)
+        {
+            return NullLogger.Instance;
+        }
+    }
+}
