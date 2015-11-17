31434fe37051f8212d06a289b52f7c8b8b0d6b61
windfierce <449570293@qq.com>
2015-11-17 17:46:28 +0800

    NextAll

--- DChat/DChat.Model/Models/MsgItem.cs
+++ DChat/DChat.Model/Models/MsgItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +12,14 @@ namespace DChat.Model.Models
     {
         public Guid Id { get; set; }
         public int UserId { get; set; }
-        //public Guid ParentId { get; set; }
         public string MsgContent { get; set; }
         public DateTime SendTime { get; set; }
 
         public virtual MsgItem Next { get; set; }
 
         public virtual MsgItem Pre { get; set; }
-
+        
+        [NotMapped]
         public IList<MsgItem> NextAll
         {
 
