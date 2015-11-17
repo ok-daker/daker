edbd034df86a73d43564a3752bcfdd76a9486f38
on-looker <694777627@qq.com>
2015-11-17 15:13:59 +0800

    修改前端样式

--- DChat/DChat.Web/Controllers/HomeController.cs
+++ DChat/DChat.Web/Controllers/HomeController.cs
@@ -23,21 +23,25 @@ namespace DChat.Web.Controllers
             userInfo.Name = "windfierce";
             //将表加入数据库
             db.UserInfos.Add(userInfo);
-            MsgItem msg = new MsgItem();
-            msg.Id = Guid.NewGuid();
-            msg.MsgContent = "aaa";
-            msg.SendTime=DateTime.Now;
-            msg.UserId = 1;
+            MsgItem msg = new MsgItem
+            {
+                Id = Guid.NewGuid(),
+                MsgContent = "aaa",
+                SendTime = DateTime.Now,
+                UserId = 1
+            };
             msg.Pre = msg;
 
             db.MsgItems.Add(msg);
             db.SaveChanges();
-            MsgItem msg2 = new MsgItem();
-            msg2.Id = Guid.NewGuid();
-            msg2.MsgContent = "aaa";
-            msg2.SendTime = DateTime.Now;
-            msg2.UserId = 1;
-            msg2.Pre = msg;
+            MsgItem msg2 = new MsgItem
+            {
+                Id = Guid.NewGuid(),
+                MsgContent = "aaa",
+                SendTime = DateTime.Now,
+                UserId = 1,
+                Pre = msg
+            };
             db.MsgItems.Add(msg2);
             db.SaveChanges();
 
