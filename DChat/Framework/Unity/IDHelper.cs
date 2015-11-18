be94a8f582bc5e14d02e7efab911f001261d77b1
windfierce <449570293@qq.com>
2015-11-18 10:45:13 +0800

    NextAll思路

--- DChat/Framework/Unity/IDHelper.cs
+++ DChat/Framework/Unity/IDHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Permissions;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 namespace DChat.Framework.Unity
 {
@@ -12,6 +13,7 @@ namespace DChat.Framework.Unity
     {
         private static object _root = new object();        
         public static MsgItem _preMsgItem;
+        public static IEnumerable<MsgItem> _nextAllItem;
 
         static IDHelper()
         {
@@ -35,6 +37,32 @@ namespace DChat.Framework.Unity
         public static MsgItem GetPreItem()
         {
             return new MsgItem { };
+        }
+
+        public static IEnumerable<MsgItem> GetNextAll(MsgItem msgItem)
+        {
+            if (IsGuid(msgItem.Id.ToString()))
+            {
+                //查询数据库 得到所有以当前msgItem.Id为parent_id的MsgItems 发送时间升序排列 并赋值给_nextAllItem
+            }
+            return new List<MsgItem>();
+        }
+        /// <summary>
+        /// 判断MsgItem.Id是否为GUID
+        /// </summary>
+        /// <param name="strSrc"></param>
+        /// <returns></returns>
+        private static bool IsGuid(string strSrc)
+        {
+            if (string.IsNullOrEmpty(strSrc))
+            {
+                return false;
+            }
+            else
+            {
+                Regex reg = new Regex("^[A-F0-9]{8}(-[A-F0-9]{4}){3}-[A-F0-9]{12}$", RegexOptions.Compiled);
+                return reg.IsMatch(strSrc);
+            }
         }
     }
 }
