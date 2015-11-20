33bfa8abf8d56e8ad591768d95cd135c7b6dea4c
on-looker <694777627@qq.com>
2015-11-20 15:43:09 +0800

    save

--- DChat/DChat.Core/implement/MsgHandler.cs
+++ DChat/DChat.Core/implement/MsgHandler.cs
@@ -25,6 +25,7 @@ namespace DChat.Core.implement
             msg = MsgHelper.GetMsgItem(msg);
             Queue<MsgItem> msgs = cache.Get<Queue<MsgItem>>(MsgKey)?? new Queue<MsgItem>();
             msgs.Enqueue(msg);
+            cache.Add(MsgKey,msgs);
         }
 
         public IEnumerable<MsgItem> Get(Guid? id)
