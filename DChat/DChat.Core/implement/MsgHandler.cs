27d031f86fa9ad541e49605c09d93a6938e8cd8d
windfierce <449570293@qq.com>
2015-11-20 14:52:44 +0800

    修改MSgHandler

--- DChat/DChat.Core/implement/MsgHandler.cs
+++ DChat/DChat.Core/implement/MsgHandler.cs
@@ -19,6 +19,7 @@ namespace DChat.Core.implement
         private const int MaxLength = 50;
         private const string OnlineUserKey = "User_Online";
         private const string MsgKey = "InChatingMessages";
+        private const string DBKey = "ReadyToDB";
         public void Push(MsgItem msg)
         {
             msg = MsgHelper.GetMsgItem(msg);
@@ -29,8 +30,19 @@ namespace DChat.Core.implement
         public IEnumerable<MsgItem> Get(Guid? id)
         {
             Queue<MsgItem> msgs = cache.Get<Queue<MsgItem>>(MsgKey) ?? new Queue<MsgItem>();
+            //判断当前请求Id在不在缓存对列中
+            //if (msgs.AsEnumerable<MsgItem>().Where(m => m.Id == id).Count() > 0)            
             int index = (id == null) ? -1 : msgs.Select(m => m.Id).ToList().IndexOf(id.Value);
-            return msgs.Skip(index + 1).Take(50 - index).AsEnumerable();
+            if (index != -1)
+            {
+
+                return msgs.Skip(index + 1).Take(50 - index).AsEnumerable();
+            }
+            //不在则返回缓存中最新的10条记录
+            else
+            {
+                return msgs.Skip(MaxLength - 10).Take(10).AsEnumerable();
+            }
         }
     }
 }
