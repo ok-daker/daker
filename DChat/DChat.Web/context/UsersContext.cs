f97016243d234c285e8ca25d991e50981cbfdeae
on-looker <694777627@qq.com>
2015-11-25 10:06:48 +0800

    添加context

---
+++ DChat/DChat.Web/context/UsersContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DChat.Model.Models;
+
+namespace DChat.Web.context
+{
+    public static class UsersContext
+    {
+        private static List<UserInfo> _users = new List<UserInfo>();
+
+        public static List<UserInfo> Users
+        {
+            get { return _users; }
+            private set { _users = value; }
+        }
+
+        public static void AddOnLine(UserInfo usr)
+        {
+            _users.Add(usr);
+        }
+
+        public static void SetOffLine(int id)
+        {
+            if (_users.Count(u => u.Id == id) > 0)
+            {
+                _users.Remove(_users.FirstOrDefault(u => u.Id == id));
+            }
+        }
+    }
+}
